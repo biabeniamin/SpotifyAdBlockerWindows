def35d6d777beb30e3514012879aa13f92b6f5ba
Beniamin Bia <biabeniamin@outlook.com>
2020-05-19 22:26:11 +0300

    run spotify ad blocker on a separate thread

--- SpotifyAdBlockerWindows/SpotifyAdBlockerWindows.cpp
+++ SpotifyAdBlockerWindows/SpotifyAdBlockerWindows.cpp
@@ -3,7 +3,27 @@
 
 Spotify spotify;
 
+DWORD WINAPI SpotifyAdsBlockerProc(__in LPVOID lpParameter)
+{
+	while (1)
+	{
+	}
+
+	return 0;
+}
+
+void StartSpotifyAdsBlocker()
+{
+	CreateThread(NULL,
+		0,
+		SpotifyAdsBlockerProc,
+		0,
+		0,
+		NULL);
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Starting spotify ad blocker!\n";
+	StartSpotifyAdsBlocker();
 }
